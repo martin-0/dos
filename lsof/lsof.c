ef3de3e440f5c9b181da4f7171c32653681df7a0
martin <martin@triveverky.cz>
2019-03-25 23:08:07 +0100

    minor changes

--- lsof/lsof.c
+++ lsof/lsof.c
@@ -13,6 +13,7 @@
 #include <conio.h>
 #include <dos.h>
 #include <mem.h>
+#include <string.h>
 
 /*
  * sft_size is not part of the SFT header; varies between DOS versions
@@ -35,27 +36,31 @@ struct fcb_idx {
 };
 
 char far* get_sft(void);
+char far* get_invars(void);
 
 void get_sftvars(struct fcb_idx* fdx);
 void print_header(struct fcb_idx* fdx);
 
-
-int main(void) {
+int main(int argc, char** argv) {
 	struct sft_header sfth;
 	struct fcb_idx fdx;
 	char far *SFT;
+	char far *invars;
 
 	unsigned int ref_count;
-	int i,j,sum;
+	int i,j,sum,verbose;
 	char far *fname;
 	char buf[16];
-
-	clrscr();
 
 	get_sftvars(&fdx);
 	if (fdx.dosver < 2) {
 		printf ("DOS version %d not supported\n", fdx.dosver);
 		return 1;
+	}
+
+	verbose = 0;
+	if (argc == 2) {
+		if (!strcmp(argv[1], "-v")) verbose = 1;
 	}
 
 	print_header(&fdx);
@@ -68,16 +73,16 @@ int main(void) {
 		sfth.segm = *(int far*)(SFT+2);
 		sfth.entries = *(int far*)(SFT+4);
 
-		printf ("\nSFT @ %Fp, entries: %d\n", SFT,sfth.entries);
+		printf ("\n SFT @ %Fp, entries: %d\n --------------------\n", SFT,sfth.entries);
 
 		for (i= 0; i < sfth.entries; i++) {
 			ref_count = *(int far*)(SFT+i*fdx.entrysz+0x6);
 
-			// in DOS 2.x ref_count is char
+			// in DOS 2.x ref_count is byte
 			if (fdx.dosver == 2) ref_count &= 0xff;
 
 			if (ref_count == 0) {
-				printf ("%d : free\n",sum+i);
+				if (verbose) printf ("%d : free\n",sum+i);
 			}
 			else {
 				fname = (char far*)(SFT+i*fdx.entrysz+fdx.fname_offset);
@@ -87,8 +92,7 @@ int main(void) {
 				for (j = 0; j < 11; j++) {
 					buf[j] = fname[j];
 				}
-
-				printf("%d : %s , ref: %d\n", sum+i,buf,ref_count);
+				printf("  %d : %s  %d\n", sum+i,buf,ref_count);
 			}
 		}
 		sum += sfth.entries;
@@ -98,13 +102,13 @@ int main(void) {
 
 		SFT = (char far*)MK_FP(sfth.segm, sfth.ofst);
 	}
-	printf ("FILES=%d\n", sum);
+	printf ("\nFILES=%d\n", sum);
 	return 0;
 }
 
 void print_header(struct fcb_idx* fdx) {
-	printf ("DOS version: %d\nSFT entry size: %d\nfile name offset: %d\n\n",
-		fdx->dosver, fdx->entrysz, fdx->fname_offset);
+	printf ("\nDOS version:\t%d\nin_vars:\t%Fp\nSFT entry size:\t%d\nfname offset:\t%d\n",
+		fdx->dosver,get_invars(),fdx->entrysz,fdx->fname_offset);
 }
 
 void get_sftvars(struct fcb_idx* fdx) {
@@ -157,7 +161,7 @@ char far* get_sft() {
 		push bx
 
 		mov ah, 0x52			// AH=52h get list of lists
-		int 0x21
+		int 0x21				// ES:BX invars
 
 		mov di, [es:bx+4]		// SFT table start dword
 		mov es, [es:bx+6]
@@ -171,4 +175,16 @@ char far* get_sft() {
 		xor ax,ax
 	}
 	return sft;
+}
+
+char far* get_invars(void) {
+	char far* invars;
+	asm {
+		mov ah, 0x52
+		int 0x21
+		mov word [invars+2], es
+		mov word [invars], bx
+		xor ax,ax
+	}
+	return invars;
 }
